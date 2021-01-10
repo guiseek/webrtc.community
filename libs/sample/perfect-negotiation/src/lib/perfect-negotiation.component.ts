b4263a46f7b60186f7e02561715ae541f48de22f
Guilherme Siquinelli (guiseek) <guiseek@gmail.com>
2021-01-10 04:25:25 -0300

    cleanup: working

--- libs/sample/perfect-negotiation/src/lib/perfect-negotiation.component.ts
+++ libs/sample/perfect-negotiation/src/lib/perfect-negotiation.component.ts
@@ -47,38 +47,12 @@ export class PerfectNegotiationComponent
       iceServers: [{ urls: 'stun:stun.stunprotocol.org:3478' }],
     })
 
-    this.dc = this.pc.createDataChannel('channel')
-
-    this.dc.addEventListener('open', () => {
-      console.log('OPEN')
-      window.setTimeout(() => {
-        this.dc.send('allloooww')
-      }, 3000)
-    })
-    this.dc.addEventListener('message', ({ data }) =>
-      console.log('MESSAGE 1: ', data)
-    )
-    this.dc.addEventListener('error', (err) => console.log('ERROR: ', err))
-    this.dc.addEventListener('close', () => console.log('CLOSE'))
-    this.dc.addEventListener('bufferedamountlow', (evt) =>
-      console.log('BUFFER: ', evt)
-    )
-
-    this.pc.addEventListener('datachannel', ({ channel }) => {
-      const receiveChannel = channel
-      receiveChannel.addEventListener('message', ({ data }) => {
-        console.log('MESSAGE 2: ', data);
-      })
-      receiveChannel.addEventListener('open', () => console.log('OPEN'))
-      receiveChannel.addEventListener('error', (err) => console.log('ERROR: ', err))
-      receiveChannel.addEventListener('close', () => console.log('CLOSE'))
-      this.receiver = receiveChannel
-    })
-
     this.pc.addEventListener('track', ({ track, streams }) => {
       // assim que a mídia para uma trilha remota chegar, mostre-a no elemento de vídeo remoto
       track.addEventListener('unmute', () => {
-        this.setVideo(this.remoteView, streams)
+        // não defina srcObject novamente se já estiver definido.
+        if (this.remoteView.srcObject) return
+        this.remoteView.srcObject = streams[0]
         this.active.next(true)
       })
     })
@@ -88,3 +62,94 @@ export class PerfectNegotiationComponent
     this.start()
   }
 }
+
+// import { PerfectNegotiation } from './perfect-negotiation'
+// import { SignalingChannel } from '@quertc/core'
+// import { MediaService } from '@quertc/meeting'
+// import { code } from '@quertc/controls'
+// import { Subject } from 'rxjs'
+// import {
+//   AfterViewInit,
+//   Component,
+//   ElementRef,
+//   OnDestroy,
+//   ViewChild,
+// } from '@angular/core'
+
+// @Component({
+//   selector: 'quertc-perfect-negotiation',
+//   templateUrl: './perfect-negotiation.component.html',
+//   styleUrls: ['./perfect-negotiation.component.scss'],
+// })
+// export class PerfectNegotiationComponent
+//   extends PerfectNegotiation
+//   implements AfterViewInit, OnDestroy {
+//   active = new Subject<boolean>()
+//   active$ = this.active.asObservable()
+
+//   @ViewChild('selfView') selfViewRef: ElementRef<HTMLVideoElement>
+
+//   @ViewChild('remoteView') remoteViewRef: ElementRef<HTMLVideoElement>
+
+//   constructor(
+//     protected signaling: SignalingChannel,
+//     protected stream: MediaService
+//   ) {
+//     super(signaling, stream)
+//   }
+
+//   restart = async () => {
+//     this.offerOptions.iceRestart = true
+//     this.makeOffer(this.offerOptions)
+//   }
+
+//   ngAfterViewInit() {
+//     this.sender = code()
+//     this.selfView = this.selfViewRef.nativeElement
+//     this.remoteView = this.remoteViewRef.nativeElement
+
+//     this.pc = new RTCPeerConnection({
+//       iceServers: [{ urls: 'stun:stun.stunprotocol.org:3478' }],
+//     })
+
+//     this.dc = this.pc.createDataChannel('channel')
+
+//     this.dc.addEventListener('open', () => {
+//       console.log('OPEN')
+//       window.setTimeout(() => {
+//         this.dc.send('allloooww')
+//       }, 3000)
+//     })
+//     this.dc.addEventListener('message', ({ data }) =>
+//       console.log('MESSAGE 1: ', data)
+//     )
+//     this.dc.addEventListener('error', (err) => console.log('ERROR: ', err))
+//     this.dc.addEventListener('close', () => console.log('CLOSE'))
+//     this.dc.addEventListener('bufferedamountlow', (evt) =>
+//       console.log('BUFFER: ', evt)
+//     )
+
+//     this.pc.addEventListener('datachannel', ({ channel }) => {
+//       const receiveChannel = channel
+//       receiveChannel.addEventListener('message', ({ data }) => {
+//         console.log('MESSAGE 2: ', data);
+//       })
+//       receiveChannel.addEventListener('open', () => console.log('OPEN'))
+//       receiveChannel.addEventListener('error', (err) => console.log('ERROR: ', err))
+//       receiveChannel.addEventListener('close', () => console.log('CLOSE'))
+//       this.receiver = receiveChannel
+//     })
+
+//     this.pc.addEventListener('track', ({ track, streams }) => {
+//       // assim que a mídia para uma trilha remota chegar, mostre-a no elemento de vídeo remoto
+//       track.addEventListener('unmute', () => {
+//         this.setVideo(this.remoteView, streams)
+//         this.active.next(true)
+//       })
+//     })
+
+//     this.afterViewComplete()
+
+//     this.start()
+//   }
+// }
--- libs/sample/perfect-negotiation/src/lib/perfect-negotiation.ts
+++ libs/sample/perfect-negotiation/src/lib/perfect-negotiation.ts
@@ -1,3 +1,4 @@
+
 import { AfterViewInit, Component, Injectable, OnDestroy } from '@angular/core'
 import { PeerEvent, SignalingChannel } from '@quertc/core'
 import { MediaService } from '@quertc/meeting'
@@ -9,9 +10,7 @@ export class PerfectNegotiation implements AfterViewInit, OnDestroy {
   destroy$ = new Subject<void>()
 
   protected pc: RTCPeerConnection
-  protected dc: RTCDataChannel
   sender: string
-  receiver: RTCDataChannel
 
   /**
    * manter o controle de algum estado de
@@ -38,11 +37,11 @@ export class PerfectNegotiation implements AfterViewInit, OnDestroy {
 
   start = async () => {
     try {
-      this.stream.currentStream = await this.stream.getUserMedia()
+      this.stream.currentStream = await this.stream.getUserMedia({ audio: true, video: true})
       for (const track of this.stream.currentStream.getTracks()) {
         this.pc.addTrack(track, this.stream.currentStream)
       }
-      this.setVideo(this.selfView, [this.stream.currentStream])
+      this.selfView.srcObject = this.stream.currentStream
       this.selfView.muted = true
     } catch (err) {
       console.error(err)
@@ -128,12 +127,6 @@ export class PerfectNegotiation implements AfterViewInit, OnDestroy {
       })
   }
 
-  setVideo(video: HTMLVideoElement, [stream]: readonly MediaStream[]) {
-    // não defina srcObject novamente se já estiver definido.
-    if (video.srcObject) return
-    video.srcObject = stream
-  }
-
   hangup() {
     console.log('Ending call')
     this.stream.currentStream.getTracks().forEach((t) => t.stop())
@@ -149,3 +142,153 @@ export class PerfectNegotiation implements AfterViewInit, OnDestroy {
     this.destroy$.complete()
   }
 }
+
+// import { AfterViewInit, Component, Injectable, OnDestroy } from '@angular/core'
+// import { PeerEvent, SignalingChannel } from '@quertc/core'
+// import { MediaService } from '@quertc/meeting'
+// import { takeUntil } from 'rxjs/operators'
+// import { Subject } from 'rxjs'
+
+// @Component({ template: '' })
+// export class PerfectNegotiation implements AfterViewInit, OnDestroy {
+//   destroy$ = new Subject<void>()
+
+//   protected pc: RTCPeerConnection
+//   sender: string
+
+//   /**
+//    * manter o controle de algum estado de
+//    * negociação para evitar corridas e erros
+//    */
+//   makingOffer = false
+//   ignoreOffer = false
+//   isSettingRemoteAnswerPending = false
+
+//   offerOptions: RTCOfferOptions = {
+//     offerToReceiveAudio: true,
+//     offerToReceiveVideo: true,
+//   }
+
+//   selfView: HTMLVideoElement
+//   remoteView: HTMLVideoElement
+
+//   constructor(
+//     protected signaling: SignalingChannel,
+//     protected stream: MediaService
+//   ) {}
+
+//   ngAfterViewInit() {}
+
+//   start = async () => {
+//     try {
+//       this.stream.currentStream = await this.stream.getUserMedia()
+//       for (const track of this.stream.currentStream.getTracks()) {
+//         this.pc.addTrack(track, this.stream.currentStream)
+//       }
+//       this.setVideo(this.selfView, [this.stream.currentStream])
+//       this.selfView.muted = true
+//     } catch (err) {
+//       console.error(err)
+//     }
+//   }
+
+//   async makeOffer(options?: RTCOfferOptions) {
+//     try {
+//       this.makingOffer = true
+//       await this.pc.setLocalDescription(await this.pc.createOffer(options))
+//       if (this.pc.localDescription) {
+//         this.signaling.send({
+//           sender: this.sender,
+//           description: this.pc.localDescription,
+//         })
+//       }
+//     } catch (err) {
+//       console.error(err)
+//     } finally {
+//       this.makingOffer = false
+//     }
+//   }
+
+//   //  A lógica de negociação perfeita, separada do resto da aplicação
+//   afterViewComplete() {
+//     // enviar qualquer candidato de gelo para o outro par
+//     this.pc.addEventListener('icecandidate', ({ candidate }) => {
+//       if (candidate) {
+//         this.signaling.send({ sender: this.sender, candidate })
+//       }
+//     })
+
+//     // deixe o evento "necessário para a negociação" gerar a oferta
+//     this.pc.addEventListener('negotiationneeded', async () => {
+//       this.makeOffer(this.offerOptions)
+//     })
+
+//     this.signaling.message$
+//       .pipe(takeUntil(this.destroy$))
+//       .subscribe(async ({ sender, description, candidate }) => {
+//         console.log('sender: ', sender)
+//         try {
+//           if (description) {
+//             // Uma oferta pode chegar enquanto estamos ocupados processando SRD (resposta).
+//             // Nesse caso, estaremos "estáveis" no momento em que a oferta for processada
+//             // então é seguro encadear em nossa Cadeia de Operações agora.
+//             const readyForOffer =
+//               !this.makingOffer &&
+//               (this.pc.signalingState == 'stable' ||
+//                 this.isSettingRemoteAnswerPending)
+//             const offerCollision =
+//               description.type == PeerEvent.Offer && !readyForOffer
+
+//             const polite = sender === this.sender
+
+//             this.ignoreOffer = polite && offerCollision
+//             if (this.ignoreOffer) {
+//               return
+//             }
+//             this.isSettingRemoteAnswerPending =
+//               description.type == PeerEvent.Answer
+
+//             await this.pc.setRemoteDescription(description) // SRD reverte conforme necessário
+
+//             this.isSettingRemoteAnswerPending = false
+
+//             if (description.type == PeerEvent.Offer) {
+//               await this.pc.setLocalDescription(await this.pc.createAnswer())
+//               if (this.pc.localDescription) {
+//                 this.signaling.send({ description: this.pc.localDescription })
+//               }
+//             }
+//           } else if (candidate) {
+//             try {
+//               await this.pc.addIceCandidate(candidate)
+//             } catch (err) {
+//               if (!this.ignoreOffer) throw err // Suprimir os candidatos da oferta ignorada
+//             }
+//           }
+//         } catch (err) {
+//           console.error(err)
+//         }
+//       })
+//   }
+
+//   setVideo(video: HTMLVideoElement, [stream]: readonly MediaStream[]) {
+//     // não defina srcObject novamente se já estiver definido.
+//     if (video.srcObject) return
+//     video.srcObject = stream
+//   }
+
+//   hangup() {
+//     console.log('Ending call')
+//     this.stream.currentStream.getTracks().forEach((t) => t.stop())
+//     if (this.pc) {
+//       this.pc.close()
+//       Object.defineProperties(this.pc, {})
+//     }
+//   }
+
+//   ngOnDestroy() {
+//     this.hangup()
+//     this.destroy$.next()
+//     this.destroy$.complete()
+//   }
+// }
